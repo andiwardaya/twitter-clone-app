94b62fc9c01bddeeaf40a4424e7f8cce50680e9c
andiwardaya <andiwardaya44@gmail.com>
2022-06-03 13:35:35 +0700

    add logout button in responsive mobile screen

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,6 @@
 import { SparklesIcon } from "@heroicons/react/solid";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { signOut, useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase";
 import Input from "./Input";
@@ -33,11 +34,19 @@ const Feed = () => {
     [db]
   );
 
+  const { data: session } = useSession();
   return (
     <div className="  text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[350px]">
       {/* FEED HEADER */}
       <div className=" flex items-center sm:justify-between border-b border-gray-700 text-[#d9d9d9] px-2 py-3 sticky top-0 z-50 bg-black">
         <h4 className="text-lg sm:text-xl font-bold">Home</h4>
+        <div
+          onClick={signOut}
+          className="flex items-center space-x-2 mx-auto border border-gray-400 px-4 py-2 rounded-lg hover:bg-gray-400 hover:text-black"
+        >
+          <img className="w-8 h-8 rounded-full" src={session.user.image} />
+          <button className="font-bold">Logout</button>
+        </div>
         <div className="hoverAnimation flex items-center justify-center w-9 h-9 xl:px-0 ml-auto">
           <SparklesIcon className="h-5 text-white" />
         </div>
